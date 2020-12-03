cfd2f780070d9875bed31df3259fffdccd451fcb
GerardoRosas <rosas.ruizlg@gmail.com>
2020-12-02 22:40:11 -0600

    Creacion de usuarios desde Node

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 
 //Crear el servidor
 const app = express();
 
+//Conectar a la base de datos
+conectarDB()
+
 
 //Puerto de la App 
 const port = process.env.PORT || 4000;
 
+//Habilitar los valores de un body
+app.use( express.json())
 
+//Registrar rutas de la App
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
